76a104a3bdcb2bfc45d76a8e4ae36478e6e71160
Dima YaFlay <93622229+YaFlay@users.noreply.github.com>
2021-11-21 18:28:55 +0300

    1.0.0
    Add some function!

---
+++ ip_adress.py
@@ -0,0 +1,18 @@
+import socket
+
+HOST = os.read('my_ip.txt')  # Standard loopback interface address (localhost)
+PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    s.listen()
+    conn, addr = s.accept()
+    with conn:
+        print('Connected by', addr)
+        while True:
+            data = conn.recv(1024)
+            if not data:
+                break
+            if data == b'say hello':
+                print("hello")
+#   Thanks Victor VosMottor from stackoverflow!
