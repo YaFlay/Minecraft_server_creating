2dbda9a2aa3f86d8794b723011d028ac8770bf01
Dima YaFlay <93622229+YaFlay@users.noreply.github.com>
2022-01-03 01:54:37 +0300

    2.4.6

--- index.py
+++ index.py
@@ -5,9 +5,12 @@ from urllib.request import urlretrieve
 cwd = getcwd()
 windows = 'https://raw.githubusercontent.com/YaFlay/Minecraft_server_creating/main/windows.py'
 if not path.isfile('/windows.py'): urlretrieve(windows, str(cwd)+'/windows.py')
+#   download windows.py
 commands = 'https://raw.githubusercontent.com/YaFlay/Minecraft_server_creating/main/commands.py'
 if not path.isfile('/commands.py'): urlretrieve(commands, str(cwd)+'/commands.py')
+#   download commands.py
 installer = 'https://raw.githubusercontent.com/YaFlay/Minecraft_server_creating-BETA-/main/installer.py'
 if not path.isfile('/installer.py'): urlretrieve(installer, str(cwd)+'/installer.py')
+#   download commands.py
 system('python3 '+ str(cwd)+'/windows.py')
 
