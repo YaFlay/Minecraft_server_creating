08bb45c7d3fe10a3b623806284c9d1086e5f1931
Dima YaFlay <93622229+YaFlay@users.noreply.github.com>
2022-01-03 02:05:44 +0300

    2.4.6
    fixing link

--- index.py
+++ index.py
@@ -9,8 +9,7 @@ if not path.isfile('/windows.py'): urlretrieve(windows, str(cwd)+'/windows.py')
 commands = 'https://raw.githubusercontent.com/YaFlay/Minecraft_server_creating/main/commands.py'
 if not path.isfile('/commands.py'): urlretrieve(commands, str(cwd)+'/commands.py')
 #   download commands.py
-installer = 'https://raw.githubusercontent.com/YaFlay/Minecraft_server_creating-BETA-/main/installer.py'
+installer = 'https://raw.githubusercontent.com/YaFlay/Minecraft_server_creating/main/installer.py'
 if not path.isfile('/installer.py'): urlretrieve(installer, str(cwd)+'/installer.py')
 #   download commands.py
 system('python3 '+ str(cwd)+'/windows.py')
-
